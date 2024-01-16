faa4f601e301a37d33af2747a9d9b97638e6b79a
Alexis Rodrigo Ruiz Diaz <alexis00rodrigo@gmail.com>
2024-01-16 11:14:55 -0300

    add: projects data

---
+++ src/data/projects.js
@@ -0,0 +1,28 @@
+export const PROJECTS = [
+  {
+    id:"dailyplanner",
+    name:"DailyPlanner",
+    image:"https://i.ibb.co/9W3Qn5k/dailyplanner-1.jpg",
+    description:"fusce ut placerat orci nulla pellentesque dignissim enim sit amet venenatis urna cursus eget nunc scelerisque viverra mauris in aliquam sem fringilla ut morbi tincidunt augue interdum velit euismod in pellentesque massa placerat duis ultricies lacus sed turpis tincidunt id aliquet risus feugiat in ante metus dictum at tempor commodo ullamcorper a lacus vestibulum sed arcu non odio euismod lacinia at quis risus sed vulputate odio ut enim blandit volutpat maecenas volutpat blandit aliquam etiam erat velit scelerisque in dictum non consectetur a erat nam at lectus urna duis convallis convallis tellus id interdum velit laoreet id donec ultrices tincidunt",
+    stacks:["react", "firebase","tailwindcss"],
+    demo:"https://dailyplanner-app.netlify.app/",
+    repository:"https://github.com/alexis00rod/DailyPlanner"
+  },
+  {
+    id:"animecatalog",
+    name:"AnimeCatalog",
+    image:"https://i.ibb.co/FDGRKsf/animecatalog-1.jpg",
+    description:"fusce ut placerat orci nulla pellentesque dignissim enim sit amet venenatis urna cursus eget nunc scelerisque viverra mauris in aliquam sem fringilla ut morbi tincidunt augue interdum velit euismod in pellentesque massa placerat duis ultricies lacus sed turpis tincidunt id aliquet risus feugiat in ante metus dictum at tempor commodo ullamcorper a lacus vestibulum sed arcu non odio euismod lacinia at quis risus sed vulputate odio ut enim blandit volutpat maecenas volutpat blandit aliquam etiam erat velit scelerisque in dictum non consectetur a erat nam at lectus urna duis convallis convallis tellus id interdum velit laoreet id donec ultrices tincidunt",
+    stacks:["react", "tailwindcss"],
+    demo:"https://animecatalog.netlify.app/",
+    repository:"https://github.com/alexis00rod/AnimeCatalog"
+  },  {
+    id:"frimarket",
+    name:"FRIMarket",
+    image:"https://i.ibb.co/8z5DyX2/frimarket-1.jpg",
+    description:"fusce ut placerat orci nulla pellentesque dignissim enim sit amet venenatis urna cursus eget nunc scelerisque viverra mauris in aliquam sem fringilla ut morbi tincidunt augue interdum velit euismod in pellentesque massa placerat duis ultricies lacus sed turpis tincidunt id aliquet risus feugiat in ante metus dictum at tempor commodo ullamcorper a lacus vestibulum sed arcu non odio euismod lacinia at quis risus sed vulputate odio ut enim blandit volutpat maecenas volutpat blandit aliquam etiam erat velit scelerisque in dictum non consectetur a erat nam at lectus urna duis convallis convallis tellus id interdum velit laoreet id donec ultrices tincidunt",
+    stacks:["react", "firebase", "tailwindcss"],
+    demo:"https://frimarket.netlify.app/",
+    repository:"https://github.com/alexis00rod/FRIMarket"
+  }
+]
