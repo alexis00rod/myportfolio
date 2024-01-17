e50d48901c73c22de1f48a5e32c9e69d67e82e18
Alexis Rodrigo Ruiz Diaz <alexis00rodrigo@gmail.com>
2024-01-17 20:14:21 -0300

    add: experience data

---
+++ src/data/experience.js
@@ -0,0 +1,17 @@
+export const EXPERIENCE = [
+  {
+    title:'Carrera de Desarrollo Frontend React',
+    place:'CoderHouse',
+    date:'2021'
+  },
+  {
+    title:'Master en CSS',
+    place:'Udemy',
+    date:'2022'
+  },
+  {
+    title:'Licenciatura en Análisis de Sistemas',
+    place:'Universidad de Buenos Aires',
+    date:'2022-Actualmente'
+  },
+]
