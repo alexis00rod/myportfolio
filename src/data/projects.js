a467d285c6110d5c9822c41a4202f5d10ff56cba
Alexis Rodrigo Ruiz Diaz <alexis00rodrigo@gmail.com>
2024-01-18 11:37:21 -0300

    add: process, images and descrition props in objects

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,29 +2,53 @@ export const PROJECTS = [
   {
     id:"dailyplanner",
     name:"DailyPlanner",
-    image:"https://i.ibb.co/9W3Qn5k/dailyplanner-1.jpg",
+    images:[
+      "https://i.ibb.co/9W3Qn5k/dailyplanner-1.jpg"
+    ],
     subtitle: "facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui",
-    description:"fusce ut placerat orci nulla pellentesque dignissim enim sit amet venenatis urna cursus eget nunc scelerisque viverra mauris in aliquam sem fringilla ut morbi tincidunt augue interdum velit euismod in pellentesque massa placerat duis ultricies lacus sed turpis tincidunt id aliquet risus feugiat in ante metus dictum at tempor commodo ullamcorper a lacus vestibulum sed arcu non odio euismod lacinia at quis risus sed vulputate odio ut enim blandit volutpat maecenas volutpat blandit aliquam etiam erat velit scelerisque in dictum non consectetur a erat nam at lectus urna duis convallis convallis tellus id interdum velit laoreet id donec ultrices tincidunt",
-    stacks:["react", "firebase","tailwindcss"],
+    description:[
+      "fusce ut placerat orci nulla pellentesque dignissim enim sit amet venenatis urna cursus eget nunc scelerisque viverra mauris in aliquam sem fringilla ut morbi tincidunt augue interdum velit euismod in pellentesque massa placerat duis ultricies lacus sed turpis tincidunt id aliquet risus feugiat in ante metus dictum at tempor commodo ullamcorper a lacus vestibulum sed arcu non odio euismod lacinia at quis risus sed vulputate odio ut enim blandit volutpat maecenas volutpat blandit aliquam etiam erat velit scelerisque in dictum non consectetur a erat nam at lectus urna duis convallis convallis tellus id interdum velit laoreet id donec ultrices tincidunt"
+    ],
+    process: "",
+    stacks:["react", "firebase","Tailwind CSS"],
     demo:"https://dailyplanner-app.netlify.app/",
     repository:"https://github.com/alexis00rod/DailyPlanner"
   },
   {
     id:"animecatalog",
     name:"AnimeCatalog",
-    image:"https://i.ibb.co/FDGRKsf/animecatalog-1.jpg",
+    images:[
+      "https://i.ibb.co/FDGRKsf/animecatalog-1.jpg"
+    ],
     subtitle: "facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui",
-    description:"fusce ut placerat orci nulla pellentesque dignissim enim sit amet venenatis urna cursus eget nunc scelerisque viverra mauris in aliquam sem fringilla ut morbi tincidunt augue interdum velit euismod in pellentesque massa placerat duis ultricies lacus sed turpis tincidunt id aliquet risus feugiat in ante metus dictum at tempor commodo ullamcorper a lacus vestibulum sed arcu non odio euismod lacinia at quis risus sed vulputate odio ut enim blandit volutpat maecenas volutpat blandit aliquam etiam erat velit scelerisque in dictum non consectetur a erat nam at lectus urna duis convallis convallis tellus id interdum velit laoreet id donec ultrices tincidunt",
-    stacks:["react", "tailwindcss"],
+    description:[
+      "fusce ut placerat orci nulla pellentesque dignissim enim sit amet venenatis urna cursus eget nunc scelerisque viverra mauris in aliquam sem fringilla ut morbi tincidunt augue interdum velit euismod in pellentesque massa placerat duis ultricies lacus sed turpis tincidunt id aliquet risus feugiat in ante metus dictum at tempor commodo ullamcorper a lacus vestibulum sed arcu non odio euismod lacinia at quis risus sed vulputate odio ut enim blandit volutpat maecenas volutpat blandit aliquam etiam erat velit scelerisque in dictum non consectetur a erat nam at lectus urna duis convallis convallis tellus id interdum velit laoreet id donec ultrices tincidunt"
+    ],
+    process: "",
+    stacks:["react", "Tailwind CSS"],
     demo:"https://animecatalog.netlify.app/",
     repository:"https://github.com/alexis00rod/AnimeCatalog"
   },  {
     id:"frimarket",
     name:"FRIMarket",
-    image:"https://i.ibb.co/8z5DyX2/frimarket-1.jpg",
-    subtitle: "facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui",
-    description:"fusce ut placerat orci nulla pellentesque dignissim enim sit amet venenatis urna cursus eget nunc scelerisque viverra mauris in aliquam sem fringilla ut morbi tincidunt augue interdum velit euismod in pellentesque massa placerat duis ultricies lacus sed turpis tincidunt id aliquet risus feugiat in ante metus dictum at tempor commodo ullamcorper a lacus vestibulum sed arcu non odio euismod lacinia at quis risus sed vulputate odio ut enim blandit volutpat maecenas volutpat blandit aliquam etiam erat velit scelerisque in dictum non consectetur a erat nam at lectus urna duis convallis convallis tellus id interdum velit laoreet id donec ultrices tincidunt",
-    stacks:["react", "firebase", "tailwindcss"],
+    images:[
+      "/src/assets/frimarket/frimarket1.jpeg",
+      "/src/assets/frimarket/frimarket2.jpeg",
+      "/src/assets/frimarket/frimarket3.jpeg",
+      "/src/assets/frimarket/frimarket4.jpeg",
+      "/src/assets/frimarket/frimarket5.jpeg",
+      "/src/assets/frimarket/frimarket6.jpeg",
+      "/src/assets/frimarket/frimarket7.jpeg",
+      "/src/assets/frimarket/frimarket8.jpeg",
+    ],
+    subtitle: "Explora un nuevo horizonte de compras y ventas, donde la simplicidad se encuentra con la diversidad",
+    description:[
+      "Descubre FRIMarket, tu mercado virtual donde la simplicidad se encuentra con la diversidad.",
+      "Con un registro sencillo, sumérgete en un mundo lleno de posibilidades.",
+      "Publica tus productos con facilidad, presentando imágenes llamativas y descripciones detalladas.",
+      "Explora una amplia variedad de productos, desde artículos de segunda mano hasta productos nuevos y únicos."],
+    process: "FRIMarket es un sitio web creado con ReactJS, Tailwind CSS y Firebase, diseñado para adaptarse a diversos dispositivos. Incorpora Moment.js para gestionar fechas y utiliza la API Georef para funciones basadas en ubicación. Además, incluye tipografías de Google Fonts e iconos de FontAwesome.",
+    stacks:["React", "Firebase", "Tailwind CSS"],
     demo:"https://frimarket.netlify.app/",
     repository:"https://github.com/alexis00rod/FRIMarket"
   }
